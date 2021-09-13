d986ddead5af34e20254d0726ea207538c702c5e
chidimi <daichi.imai@hotmail.com>
2021-09-14 01:52:20 +0900

    不要なコード削除

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,9 @@ import React, { useState } from 'react';
 import './App.css';
 import ModalButton from './components/ModalButton';
 import './index.css';
-import { FcBusinessman, FcBusinesswoman, FcAssistant, FcLike, FcElectricity, FcHeadset, FcLinux, FcStart, FcVip } from 'react-icons/fc'
 import { IconType } from 'react-icons';
 import { IconContext } from 'react-icons/lib';
 
-
-const icons = {
-  fcBusinessman: FcBusinessman,
-  fcBusinesswoman: FcBusinesswoman,
-  fcAssistant: FcAssistant,
-  fcLike: FcLike,
-  fcElectricity: FcElectricity,
-  fcHeadset: FcHeadset,
-  fcLinux: FcLinux,
-  fcStart: FcStart,
-  fcVip: FcVip
-}
 
 function App() : JSX.Element {
   const [icon, setIcon] = useState<IconType>();
