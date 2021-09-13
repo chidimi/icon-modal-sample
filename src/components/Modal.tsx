89eb0c532df600e858c8ae0f10fe51b67900c121
chidimi <daichi.imai@hotmail.com>
2021-09-14 00:50:38 +0900

    iconのテキスト修正

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -16,9 +16,7 @@ export default function Modal({ show, setShow } : Props): JSX.Element | null {
           <span className="hidden sm:inline-block sm:align-middle sm:h-screen">
             <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
               <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                <div className="sm:flex sm:items-start">
-                  <h3 className="text-lg leading-6 font-medium text-gray-900">icons</h3>
-                </div>
+                  <h3 className="text-center text-3xl leading-6 font-medium text-gray-900">icons</h3>
                   <Icons />
               </div>
                 <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
